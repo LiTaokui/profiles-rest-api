53a9b8e4a88b588e8d93e7b4a2d8d9ba4c2c9706
LI TAOKUI <1139224958@qq.com>
2023-05-12 09:51:03 +0800

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializers_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLS',
+        ]
+
+        return Response({'message': 'hello', 'an_apiview': an_apiview})
